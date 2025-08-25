4b273494fd437e5e7fb5295be666d7e70a510184
projectmydsrt-bro <projectmydsrt@gmail.com>
2025-08-25 21:30:32 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,18 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <main className="text-center mt-20">
+        <h1 className="text-5xl font-bold">DSRT</h1>
+        <p className="text-xl mt-4">Digital Smart Revise Technology</p>
+        <button className="mt-6 px-6 py-3 bg-blue-600 text-white rounded-lg">
+          Get Started
+        </button>
+      </main>
+      <Footer />
+    </div>
+  );
+    }
